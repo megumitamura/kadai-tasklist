46cc185d5f26a50fa4cd5a7df0dd76e4659e6564
megumi.tamura <ec2-user@ip-172-31-34-87.us-east-2.compute.internal>
2018-04-09 17:13:12 +0000

    status

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -50,10 +50,12 @@ class TasksController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
+            'status' => 'required|max:255',   // 追加
             'content' => 'required|max:255',
         ]);
 
         $task = new Task;
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
@@ -102,10 +104,12 @@ class TasksController extends Controller
     public function update(Request $request, $id)
     {
         $this->validate($request, [
+            'status' => 'required|max:255',   // 追加
             'content' => 'required|max:255',
         ]);
         
         $task = Task::find($id);
+        $task->status = $request->status;    // 追加
         $task->content = $request->content;
         $task->save();
 
