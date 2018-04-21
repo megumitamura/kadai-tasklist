ce0799d92af7bd3640402badc299d9c2a1d04212
megumi.tamura <ec2-user@ip-172-31-34-87.us-east-2.compute.internal>
2018-04-21 10:32:17 +0000

    migration-v

--- database/migrations/2018_04_21_185831_add_user_id_to_tasks_table.php
+++ database/migrations/2018_04_21_185831_add_user_id_to_tasks_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddTitleToTasksTable extends Migration
+class AddUserIdToTasksTable extends Migration
 {
     /**
      * Run the migrations.
